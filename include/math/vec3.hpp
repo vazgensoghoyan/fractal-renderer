fc9c961eb8e0a4d3ff12d3d1853f06c34c996e77
vazgensoghoyan <sogvazulchik@gmail.com>
2025-12-27 20:42:22 +0300

    vec3 refactor

--- include/math/vec3.hpp
+++ include/math/vec3.hpp
@@ -1,49 +1,59 @@
-#pragma once // vec3.hpp
+#pragma once
 
 #include <cmath>
+#include <ostream>
 
 namespace iheay::math {
 
-    class Vec3 {
+class Vec3 {
+public:
+    constexpr Vec3() : m_x(0), m_y(0), m_z(0) { }
+    constexpr Vec3(double x, double y, double z) : m_x(x), m_y(y), m_z(z) { }
 
-    public:
-        Vec3(double x, double y, double z) : m_x(x), m_y(y), m_z(z) { }
+    constexpr double x() const { return m_x; }
+    constexpr double y() const { return m_y; }
+    constexpr double z() const { return m_z; }
 
-        double get_x() const { return m_x; }
-        double get_y() const { return m_y; }
-        double get_z() const { return m_z; }
+    double length() const { return std::sqrt(length_squared()); }
+    double length_squared() const { return m_x * m_x + m_y * m_y + m_z * m_z; }
 
-        double get_length() const { return sqrt(m_x * m_x + m_y * m_y + m_z * m_z); }
+    Vec3 normalized() const {
+        double len = length();
+        if (len == 0) return Vec3(0,0,0);
+        return *this / len;
+    }
 
-        Vec3 operator-() const { 
-            return Vec3(-m_x, -m_y, -m_z); 
-        }
+    Vec3 operator-() const { return Vec3(-m_x, -m_y, -m_z); }
 
-        Vec3 operator+(const Vec3& o) const { 
-            return Vec3(m_x + o.m_x, m_y + o.m_y, m_z + o.m_z); 
-        }
+    Vec3 operator+(const Vec3& o) const { return Vec3(m_x + o.m_x, m_y + o.m_y, m_z + o.m_z); }
+    Vec3 operator-(const Vec3& o) const { return Vec3(m_x - o.m_x, m_y - o.m_y, m_z - o.m_z); }
+    Vec3 operator*(double scalar) const { return Vec3(m_x * scalar, m_y * scalar, m_z * scalar); }
+    Vec3 operator/(double scalar) const { return Vec3(m_x / scalar, m_y / scalar, m_z / scalar); }
 
-        Vec3 operator-(const Vec3& o) const { 
-            return *this + (-o); 
-        }
+    Vec3& operator+=(const Vec3& o) { m_x += o.m_x; m_y += o.m_y; m_z += o.m_z; return *this; }
+    Vec3& operator-=(const Vec3& o) { m_x -= o.m_x; m_y -= o.m_y; m_z -= o.m_z; return *this; }
+    Vec3& operator*=(double scalar) { m_x *= scalar; m_y *= scalar; m_z *= scalar; return *this; }
+    Vec3& operator/=(double scalar) { m_x /= scalar; m_y /= scalar; m_z /= scalar; return *this; }
 
-        Vec3 operator*(double scalar) const { 
-            return Vec3(m_x * scalar, m_y * scalar, m_z * scalar); 
-        }
+    double dot(const Vec3& o) const { return m_x * o.m_x + m_y * o.m_y + m_z * o.m_z; }
+    Vec3 cross(const Vec3& o) const {
+        return Vec3(
+            m_y * o.m_z - m_z * o.m_y,
+            m_z * o.m_x - m_x * o.m_z,
+            m_x * o.m_y - m_y * o.m_x
+        );
+    }
 
-        Vec3 operator/(double scalar) const {
-            return Vec3(m_x / scalar, m_y / scalar, m_z / scalar);
-        }
+    friend std::ostream& operator<<(std::ostream& os, const Vec3& v) {
+        return os << '(' << v.m_x << ", " << v.m_y << ", " << v.m_z << ')';
+    }
 
-        Vec3 normalize() const {
-            return *this / get_length();
-        }
+private:
+    double m_x, m_y, m_z;
+};
 
-    private:
-        double m_x;
-        double m_y;
-        double m_z;
-
-    };
-
+inline Vec3 operator*(double scalar, const Vec3& v) {
+    return v * scalar;
 }
+
+} // namespace iheay::math
