1537a3506080f6b4b2508ac1bc7c64a43dc9deaf
vazgensoghoyan <sogvazulchik@gmail.com>
2025-12-30 01:54:52 +0300

    refactor of classes Ray and Vec3

--- include/math/ray.hpp
+++ include/math/ray.hpp
@@ -2,17 +2,37 @@
 
 #include "math/vec3.hpp"
 
+#include <cassert>
+
 namespace iheay::math {
 
 class Ray {
 public:
-    Ray() {}
-    Ray(Vec3 origin, Vec3 direction) : m_origin(origin), m_direction(direction) {}
+    Ray() = delete;
 
-    const Vec3& origin() const { return m_origin; }
-    const Vec3& direction() const { return m_direction; }
+    constexpr Ray(const Vec3& origin, const Vec3& direction) noexcept
+        : m_origin(origin),
+          m_direction(normalize_checked(direction))
+    {}
 
-    Vec3 at(double t) const { return m_origin + t * m_direction; }
+    [[nodiscard]] constexpr const Vec3& origin() const noexcept {
+        return m_origin;
+    }
+
+    [[nodiscard]] constexpr const Vec3& direction() const noexcept {
+        return m_direction;
+    }
+
+    [[nodiscard]] constexpr Vec3 at(double t) const noexcept {
+        return m_origin + t * m_direction;
+    }
+
+private:
+    static constexpr Vec3 normalize_checked(const Vec3& v) noexcept {
+        const double len = v.length_squared();
+        assert(len > 0 && "Ray direction must be non-zero");
+        return v / std::sqrt(len);
+    }
 
 private:
     Vec3 m_origin;
--- include/math/vec3.hpp
+++ include/math/vec3.hpp
@@ -1,47 +1,109 @@
 #pragma once // math/vec3.hpp
 
 #include <cmath>
+#include <cassert>
 #include <ostream>
 
 namespace iheay::math {
 
 class Vec3 {
 public:
-    constexpr Vec3() : m_x(0), m_y(0), m_z(0) { }
-    constexpr Vec3(double x, double y, double z) : m_x(x), m_y(y), m_z(z) { }
+    constexpr Vec3() noexcept : m_x(0.0), m_y(0.0), m_z(0.0) {}
 
-    constexpr double x() const { return m_x; }
-    constexpr double y() const { return m_y; }
-    constexpr double z() const { return m_z; }
+    constexpr Vec3(double x, double y, double z) noexcept : m_x(x), m_y(y), m_z(z) {}
 
-    double length() const;
-    double length_squared() const;
+    [[nodiscard]] constexpr double x() const noexcept { return m_x; }
+    [[nodiscard]] constexpr double y() const noexcept { return m_y; }
+    [[nodiscard]] constexpr double z() const noexcept { return m_z; }
 
-    Vec3 normalized() const;
+    [[nodiscard]] constexpr double length_squared() const noexcept {
+        return m_x * m_x + m_y * m_y + m_z * m_z;
+    }
 
-    Vec3 operator-() const { return Vec3(-m_x, -m_y, -m_z); }
+    [[nodiscard]] double length() const noexcept {
+        return std::sqrt(length_squared());
+    }
 
-    Vec3 operator+(const Vec3& o) const { return Vec3(m_x + o.m_x, m_y + o.m_y, m_z + o.m_z); }
-    Vec3 operator-(const Vec3& o) const { return Vec3(m_x - o.m_x, m_y - o.m_y, m_z - o.m_z); }
-    Vec3 operator*(double scalar) const { return Vec3(m_x * scalar, m_y * scalar, m_z * scalar); }
-    Vec3 operator/(double scalar) const { return Vec3(m_x / scalar, m_y / scalar, m_z / scalar); }
+    [[nodiscard]] Vec3 normalized() const noexcept {
+        const double len2 = length_squared();
+        assert(len2 > 0 && "Cannot normalize 0-vector");
+        return *this / std::sqrt(len2);
+    }
 
-    Vec3& operator+=(const Vec3& o) { m_x += o.m_x; m_y += o.m_y; m_z += o.m_z; return *this; }
-    Vec3& operator-=(const Vec3& o) { m_x -= o.m_x; m_y -= o.m_y; m_z -= o.m_z; return *this; }
-    Vec3& operator*=(double scalar) { m_x *= scalar; m_y *= scalar; m_z *= scalar; return *this; }
-    Vec3& operator/=(double scalar) { m_x /= scalar; m_y /= scalar; m_z /= scalar; return *this; }
+    [[nodiscard]] constexpr Vec3 operator-() const noexcept {
+        return Vec3(-m_x, -m_y, -m_z);
+    }
 
-    static double dot(const Vec3& v1, const Vec3& v2);
-    static Vec3 cross(const Vec3& v1, const Vec3& v2);
+    [[nodiscard]] constexpr Vec3 operator+(const Vec3& o) const noexcept {
+        return Vec3(m_x + o.m_x, m_y + o.m_y, m_z + o.m_z);
+    }
+
+    [[nodiscard]] constexpr Vec3 operator-(const Vec3& o) const noexcept {
+        return Vec3(m_x - o.m_x, m_y - o.m_y, m_z - o.m_z);
+    }
+
+    [[nodiscard]] constexpr Vec3 operator*(double scalar) const noexcept {
+        return Vec3(m_x * scalar, m_y * scalar, m_z * scalar);
+    }
+
+    [[nodiscard]] constexpr Vec3 operator/(double scalar) const noexcept {
+        assert(scalar != 0 && "Division by zero in Vec3");
+        return Vec3(m_x / scalar, m_y / scalar, m_z / scalar);
+    }
+
+    constexpr Vec3& operator+=(const Vec3& o) noexcept {
+        m_x += o.m_x;
+        m_y += o.m_y;
+        m_z += o.m_z;
+        return *this;
+    }
+
+    constexpr Vec3& operator-=(const Vec3& o) noexcept {
+        m_x -= o.m_x;
+        m_y -= o.m_y;
+        m_z -= o.m_z;
+        return *this;
+    }
+
+    constexpr Vec3& operator*=(double scalar) noexcept {
+        m_x *= scalar;
+        m_y *= scalar;
+        m_z *= scalar;
+        return *this;
+    }
+
+    constexpr Vec3& operator/=(double scalar) noexcept {
+        assert(scalar != 0 && "Division by zero in Vec3");
+        m_x /= scalar;
+        m_y /= scalar;
+        m_z /= scalar;
+        return *this;
+    }
+
+    [[nodiscard]] static constexpr double dot(const Vec3& v, const Vec3& w) noexcept {
+        return v.m_x * w.m_x + v.m_y * w.m_y + v.m_z * w.m_z;
+    }
+
+    [[nodiscard]] static constexpr Vec3 cross(const Vec3& v, const Vec3& w) noexcept {
+        return Vec3(
+            v.m_y * w.m_z - v.m_z * w.m_y,
+            v.m_z * w.m_x - v.m_x * w.m_z,
+            v.m_x * w.m_y - v.m_y * w.m_x
+        );
+    }
 
 private:
-    double m_x, m_y, m_z;
+    double m_x;
+    double m_y;
+    double m_z;
 };
 
-inline Vec3 operator*(double scalar, const Vec3& v) {
+[[nodiscard]] constexpr Vec3 operator*(double scalar, const Vec3& v) noexcept {
     return v * scalar;
 }
 
-std::ostream& operator<<(std::ostream& os, const Vec3& v);
+inline std::ostream& operator<<(std::ostream& os, const Vec3& v) {
+    return os << '(' << v.x() << ", " << v.y() << ", " << v.z() << ')';
+}
 
 } // namespace iheay::math
